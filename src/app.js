58f3b13f25d9df108cf8e3f73b00ebb8a11c8f08
Aryan Vishwakarma <aryanvish86@gmail.com>
2026-02-09 09:44:43 +0000

    used ejs file as view engine

--- src/app.js
+++ src/app.js
@@ -2,11 +2,27 @@ import express from 'express';
 import adminRoutes from './routes/admin.routes.js';
 import userRoutes from './routes/user.routes.js'
 import postsRouter from './routes/posts.routes.js';
-
+import { fileURLToPath } from 'url';
+import path from "path"
 const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// View engine
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "../views"));
 
 app.get('/', (req,res) => {
-    res.send("Welcome to homepage")
+    res.render('index')
+})
+
+app.get('/profile/:id', (req,res) => {
+    res.send(`Welcome to profile page with id ${req.params.id}`)
 })
 
 app.use('/admin', adminRoutes);
